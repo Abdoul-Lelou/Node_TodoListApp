737d52c2f5de773efb5d8a66fc224d89f6ed8e04
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2024-01-04 00:39:26 +0100

    autorise the front app react

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const database = mongoose.connection
 const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "https://react-todo-list-app-fun.vercel.app/dashboard"); 
+   res.header("Access-Control-Allow-Origin", "https://react-todo-list-app-fun.vercel.app"); 
    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
    next();
