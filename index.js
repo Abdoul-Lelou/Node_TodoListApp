533b9833f2c961a7c004214b8e026cb09d3a2d36
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2024-01-04 00:29:52 +0100

    autorise the front app

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", " http://localhost:5173");
-    res.header("Access-Control-Allow-Origin", " https://react-todo-list-app-fun.vercel.app/");
+    res.header("Access-Control-Allow-Origin", " https://react-todo-list-app-fun.vercel.app/*");
 
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
